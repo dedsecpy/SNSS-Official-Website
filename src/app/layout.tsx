a4d228c5484fed9581884df0c9cdb282702efa32
dedsecpy <aejazansari242@gmail.com>
2026-06-27 18:27:12 +0530

    Update open graph image to Block A image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ const mukta = Mukta({ subsets: ["devanagari"], weight: ["400", "700", "800"], va
       siteName: "Shree Narayan Secondary School",
       images: [
         {
-          url: "/School_main.png",
+          url: "/block-a.png",
           width: 1200,
           height: 630,
           alt: "Shree Narayan Secondary School",
@@ -39,7 +39,7 @@ const mukta = Mukta({ subsets: ["devanagari"], weight: ["400", "700", "800"], va
       card: "summary_large_image",
       title: "Shree Narayan Secondary School",
       description: "Quality education from ECD to Grade 12 in Ishworpur Municipality.",
-      images: ["/School_main.png"],
+      images: ["/block-a.png"],
     },
   };
   
@@ -54,7 +54,7 @@ const mukta = Mukta({ subsets: ["devanagari"], weight: ["400", "700", "800"], va
       "name": "Shree Narayan Secondary School",
       "url": "https://www.shreenarayan.edu.np",
       "logo": "https://www.shreenarayan.edu.np/logo.jpg",
-      "image": "https://www.shreenarayan.edu.np/School_main.png",
+      "image": "https://www.shreenarayan.edu.np/block-a.png",
       "description": "Official website of Shree Narayan Secondary School, providing quality education from ECD to Grade 12 in Ishworpur Municipality, Sarlahi, Nepal.",
       "address": {
         "@type": "PostalAddress",
